349ef4535a8a30a23f28be3a32d696410268b3b6
Maxi <maximogarciamtnez@gmail.com>
2024-01-08 09:47:33 +0100

    added animation to spinner

--- packages/nimiq-icons/src/icon.ts
+++ packages/nimiq-icons/src/icon.ts
@@ -1,7 +1,8 @@
-import { IconSet, cleanupSVG, parseColors, runSVGO, isEmptyColor } from "@iconify/tools";
+import { IconSet, cleanupSVG, parseColors, runSVGO, isEmptyColor, SVG } from "@iconify/tools";
 import { ColorAttributes } from "@iconify/tools/lib/colors/attribs";
-import { compareColors, stringToColor } from '@iconify/utils/lib/colors'
+import { compareColors, stringToColor, iconToHTML } from '@iconify/utils'
 import type { Color } from '@iconify/utils/lib/colors/types'
+import { JSDOM } from "jsdom";
 
 type OptimiseIconSetOptions = {
   resetColors?: boolean,
@@ -12,7 +13,7 @@ export function optimizeIconSet(iconSet: IconSet, options?: OptimiseIconSetOptio
 }
 
 function handleIcon(iconSet: IconSet, name: string, { resetColors = false }: OptimiseIconSetOptions = {}) {
-  const svg = iconSet.toSVG(name)
+  let svg = iconSet.toSVG(name)
 
   if (!svg)
     throw new Error(`Icon ${name} is not an SVG in the ${iconSet.prefix} icon set.`);
@@ -22,6 +23,9 @@ function handleIcon(iconSet: IconSet, name: string, { resetColors = false }: Opt
 
   if (resetColors)
     parseColors(svg, { defaultColor: 'currentColor', callback: handleColors })
+
+  if (name === 'spinner')
+    svg = addSpinnerAnimation(svg)
 
   iconSet.setIcon(name, svg.getIcon())
 }
@@ -39,4 +43,23 @@ function handleColors(_: ColorAttributes, colorStr: string, color: Color | null)
     return 'currentColor'
 
   return color
+}
+
+function addSpinnerAnimation(_svg: SVG) {
+  let html = iconToHTML(_svg.getBody(), { height: `${_svg.viewBox.height}`, width: `${_svg.viewBox.width}`, viewBox: `0 0 ${_svg.viewBox.width} ${_svg.viewBox.height}` });
+
+  const svg = new JSDOM(html).window.document.querySelector('svg')!;
+
+  const doc = new JSDOM(html).window.document;
+  let animateTransform = doc.createElementNS('http://www.w3.org/2000/svg', 'animateTransform');
+  animateTransform.setAttribute('attributeName', 'transform');
+  animateTransform.setAttribute('dur', '1s');
+  animateTransform.setAttribute('type', 'rotate');
+  animateTransform.setAttribute('from', `0 0 0`);
+  animateTransform.setAttribute('to', `360 0 0`);
+  animateTransform.setAttribute('repeatCount', 'indefinite');
+  svg.appendChild(animateTransform);
+  console.log(svg.outerHTML);
+
+  return new SVG(svg.outerHTML);
 }
