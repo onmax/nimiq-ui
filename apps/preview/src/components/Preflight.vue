13e164784cd7b2e73c160ebbf86874534b98dfeb
Maxi <maximogarciamtnez@gmail.com>
2024-03-14 12:24:21 +0100

    feat: improved code inside heading

--- apps/preview/src/components/Preflight.vue
+++ apps/preview/src/components/Preflight.vue
@@ -1,59 +1,75 @@
 <template>
-  <section p-6 border-base rounded> 
+  <section p-6 border-base rounded>
     <h2>Scrollbar</h2>
-    <div mt-8  max-h-160px max-w-420px mx-auto of-auto prose class="scroll">
-      <div pb-0="!">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad obcaecati ab, est, vero laborum ea enim dolores libero dolorem dicta ipsum deserunt amet sed recusandae voluptate repellat, molestiae doloribus? Assumenda!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad obcaecati ab, est, vero laborum ea enim dolores libero dolorem dicta ipsum deserunt amet sed recusandae voluptate repellat, molestiae doloribus? Assumenda!
-        </p>
+    <div flex="~">
+
+      <div mt-8 max-h-160px max-w-420px mx-auto of-auto prose>
+        <h3 mt-0>Default scrollbar</h3>
+        <div pb-0>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad obcaecati ab, est, vero laborum ea enim dolores
+            libero dolorem dicta ipsum deserunt amet sed recusandae voluptate repellat, molestiae doloribus? Assumenda!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad obcaecati ab, est, vero laborum ea enim dolores
+            libero dolorem dicta ipsum deserunt amet sed recusandae voluptate repellat, molestiae doloribus? Assumenda!
+          </p>
+        </div>
+      </div>
+      <div mt-8 max-h-160px max-w-420px mx-auto of-auto prose class="scroll-sm">
+        <h3 mt-0><code>scroll-sm</code> scrollbar</h3>
+        <div pb-0>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad obcaecati ab, est, vero laborum ea enim dolores
+            libero dolorem dicta ipsum deserunt amet sed recusandae voluptate repellat, molestiae doloribus? Assumenda!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad obcaecati ab, est, vero laborum ea enim dolores
+            libero dolorem dicta ipsum deserunt amet sed recusandae voluptate repellat, molestiae doloribus? Assumenda!
+          </p>
+        </div>
       </div>
     </div>
   </section>
-  
-    <section mt-8 p-6 border-base rounded> 
-      <h2>Shadows</h2>
-      <div flex="~ gap-8" mt-12>
-        <div size-32 bg-neutral-200 rounded-2 shadow></div>
-        <div size-32 bg-neutral-200 rounded-2 shadow-lg></div>
-      </div>
-    </section>
 
-    <section mt-8 p-6 border-base rounded> 
-      <h2>Colors</h2>
+  <section mt-8 p-6 border-base rounded>
+    <h2>Shadows</h2>
+    <div flex="~ gap-8" mt-12>
+      <div size-32 bg-neutral-200 rounded-2 shadow></div>
+      <div size-32 bg-neutral-200 rounded-2 shadow-lg></div>
+    </div>
+  </section>
 
-      <label label>Note</label>
-      <p>
-        Colors change when you change the theme (light/dark)
-      </p>
+  <section mt-8 p-6 border-base rounded>
+    <h2>Colors</h2>
 
-      <h3 mt-4>Flat</h3>
-      <div flex="~ gap-8">
-        <div size-16 bg-neutral rounded-2></div>
-        <div size-16 bg-blue rounded-2></div>
-        <div size-16 bg-green rounded-2></div>
-        <div size-16 bg-gold rounded-2></div>
-        <div size-16 bg-red rounded-2></div>
-        <div size-16 bg-orange rounded-2></div>
-        <div size-16 bg-purple rounded-2></div>
-      </div>
+    <label label>Note</label>
+    <p>
+      Colors change when you change the theme (light/dark)
+    </p>
 
-      <h3 mt-4>Gradients</h3>
-      <div flex="~ gap-8">
-        <div size-16 bg-gradient-neutral rounded-2></div>
-        <div size-16 bg-gradient-blue rounded-2></div>
-        <div size-16 bg-gradient-green rounded-2></div>
-        <div size-16 bg-gradient-gold rounded-2></div>
-        <div size-16 bg-gradient-red rounded-2></div>
-        <div size-16 bg-gradient-orange rounded-2></div>
-      </div>
-    </section>
+    <h3 mt-4>Flat</h3>
+    <div flex="~ gap-8">
+      <div size-16 bg-neutral rounded-2></div>
+      <div size-16 bg-blue rounded-2></div>
+      <div size-16 bg-green rounded-2></div>
+      <div size-16 bg-gold rounded-2></div>
+      <div size-16 bg-red rounded-2></div>
+      <div size-16 bg-orange rounded-2></div>
+      <div size-16 bg-purple rounded-2></div>
+    </div>
+
+    <h3 mt-4>Gradients</h3>
+    <div flex="~ gap-8">
+      <div size-16 bg-gradient-neutral rounded-2></div>
+      <div size-16 bg-gradient-blue rounded-2></div>
+      <div size-16 bg-gradient-green rounded-2></div>
+      <div size-16 bg-gradient-gold rounded-2></div>
+      <div size-16 bg-gradient-red rounded-2></div>
+      <div size-16 bg-gradient-orange rounded-2></div>
+    </div>
+  </section>
 </template>
 
 
-<style>
-
-
-</style>
+<style></style>
