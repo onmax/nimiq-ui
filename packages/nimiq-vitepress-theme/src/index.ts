1c9f9c1574c6ea03b009a07589c9b29b2666f75b
onmax <maximogarciamtnez@gmail.com>
2025-03-05 15:29:44 +0100

    feat: split components and layout

--- packages/nimiq-vitepress-theme/src/index.ts
+++ packages/nimiq-vitepress-theme/src/index.ts
@@ -1,4 +1,14 @@
-import Layout from './components/Layout.vue'
+import type { App } from 'vue'
+import NqCard from './components/NqCard.vue'
+import NqGrid from './components/NqGrid.vue'
+import NqLargeCard from './components/NqLargeCard.vue'
+import Layout from './layout/Layout.vue'
 
-export { Layout }
+export { Layout, NqCard, NqGrid, NqLargeCard }
 export * from './types'
+
+export function registerNqComponents(app: App) {
+  app.component('NqCard', NqCard)
+  app.component('NqLargeCard', NqLargeCard)
+  app.component('NqGrid', NqGrid)
+}
