8516758a13c712c6624e0860e93017b35ce641dc
onmax <maximogarciamtnez@gmail.com>
2025-03-06 11:36:15 +0100

    chore: update package dependencies and improve configuration for Unocss presets

--- packages/nimiq-vitepress-theme/uno.config.ts
+++ packages/nimiq-vitepress-theme/uno.config.ts
@@ -1,20 +1,18 @@
 import { createExternalPackageIconLoader } from '@iconify/utils/lib/loader/external-pkg'
-import { defineConfig, presetAttributify, presetIcons, presetWind3, transformerDirectives } from 'unocss'
+import { defineConfig, presetIcons, transformerDirectives } from 'unocss'
 import { presetFluidSizing } from 'unocss-preset-fluid-sizing'
-import { presetScalePx } from 'unocss-preset-scale-px'
+import { presetOnmax } from 'unocss-preset-onmax'
 import { presetNimiq } from '../nimiq-css/src/index'
 
 export default defineConfig({
   presets: [
-    presetWind3(),
-    presetAttributify(),
+    presetOnmax(),
     presetNimiq({
       utilities: true,
       attributifyUtilities: true,
       fonts: false,
       typography: true,
     }),
-    presetScalePx(),
     presetFluidSizing(),
     presetIcons({
       collections: {
