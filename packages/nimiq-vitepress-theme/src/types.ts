fadb518945cbfd8326be4eabd3606b22a5c77528
onmax <maximogarciamtnez@gmail.com>
2025-03-21 10:39:20 +0100

    fix: enhance type safety for sidebar and navigation links

--- packages/nimiq-vitepress-theme/src/types.ts
+++ packages/nimiq-vitepress-theme/src/types.ts
@@ -1,28 +1,33 @@
 import type { Theme } from 'vitepress'
 
-export interface NimiqVitepressSidebar {
+export interface NimiqVitepressSidebar<T extends `/${string}/` = any> {
   label?: string
   items: {
     text: string
-    link?: string
+    link?: `${T}${string}`
     icon: string
-    items?: { text: string, link: string }[]
+    items?: { text: string, link: `${T}${string}` }[]
   }[]
 }
 
-export interface NimiqVitepressThemeNav {
+export interface NimiqVitepressThemeNav<T extends `/${string}/` = any> {
   /**
    * The string displayed in the selector.
    */
   text: string
   /**
    * The path to the sub-module. If you want to link to the root of the module, use `/`.
+   * Needs to match the folder name in your vitepress project.
    */
   subpath: string
   icon?: string
-  defaultPageLink: string
+  /**
+   * The default page link for the module. This is used to open the page when the module is selected.
+   * You need to use something like `/your-page/` or `/your-page/not-index-page`.
+   */
+  defaultPageLink: T
   description?: string
-  sidebar: NimiqVitepressSidebar[]
+  sidebar: NimiqVitepressSidebar<T>[]
 }
 
 export interface NimiqVitepressThemeConfig {
