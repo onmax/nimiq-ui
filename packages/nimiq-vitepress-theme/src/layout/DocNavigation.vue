9f9e1878318aefec98fbda91902c42322a271e95
onmax <maximogarciamtnez@gmail.com>
2025-09-17 16:53:19 +0200

    feat: add text humanization to navigation

--- packages/nimiq-vitepress-theme/src/layout/DocNavigation.vue
+++ packages/nimiq-vitepress-theme/src/layout/DocNavigation.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useDocNavigation } from '../composables/useDocNavigation'
+import { humanizeText } from '../utils/textUtils'
 
 const { navigation, showNavigation } = useDocNavigation()
 </script>
@@ -8,12 +9,12 @@ const { navigation, showNavigation } = useDocNavigation()
   <nav v-if="showNavigation && (navigation.prev || navigation.next)" grid="~ cols-1 md:cols-2 gap-8" class="nq-raw">
     <a v-if="navigation.prev" col-1 :href="navigation.prev.link" nq-hoverable h-max group gap-2>
       <div i-nimiq:arrow-left size-14 shrink-0 op="60 group-hocus:80" transition-opacity />
-      <p f-text-xl>{{ navigation.prev.text }}</p>
+      <p f-text-xl>{{ humanizeText(navigation.prev.text) }}</p>
     </a>
 
     <a v-if="navigation.next" md:col-2 :href="navigation.next.link" nq-hoverable items-end h-max group gap-2>
       <div i-nimiq:arrow-right size-14 shrink-0 op="60 group-hocus:80" transition-opacity />
-      <p f-text-xl>{{ navigation.next.text }}</p>
+      <p f-text-xl>{{ humanizeText(navigation.next.text) }}</p>
     </a>
   </nav>
 </template>
